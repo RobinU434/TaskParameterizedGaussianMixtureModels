7aa4511d76f2b493a1f02387d90a64e702b9cd14
Robin Uhrich <robin.uhrich@gmail.com>
2023-11-01 11:45:14 +0100

    added documentation

--- documentation/html/menudata.js
+++ documentation/html/menudata.js
@@ -29,19 +29,6 @@ var menudata={children:[
 {text:"Class Index",url:"classes.html"},
 {text:"Class Hierarchy",url:"inherits.html"},
 {text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html",children:[
-{text:"_",url:"functions.html#index__5F"},
-{text:"a",url:"functions.html#index_a"},
-{text:"b",url:"functions.html#index_b"},
-{text:"c",url:"functions.html#index_c"},
-{text:"d",url:"functions.html#index_d"},
-{text:"f",url:"functions.html#index_f"},
-{text:"g",url:"functions.html#index_g"},
-{text:"i",url:"functions.html#index_i"},
-{text:"m",url:"functions.html#index_m"},
-{text:"n",url:"functions.html#index_n"},
-{text:"p",url:"functions.html#index_p"},
-{text:"s",url:"functions.html#index_s"},
-{text:"w",url:"functions.html#index_w"}]},
+{text:"All",url:"functions.html"},
 {text:"Functions",url:"functions_func.html"},
 {text:"Variables",url:"functions_vars.html"}]}]}]}
--- documentation/html/search/functions_4.js
+++ documentation/html/search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['davies_5fbouldin_5fscore_32',['davies_bouldin_score',['../classtpgmm_1_1TPGMM.html#a02e915f94b3de1ac6d31c302da7b57ba',1,'tpgmm.TPGMM.davies_bouldin_score()'],['../classtpgmm_1_1utils_1_1learning__modules_1_1ClassificationModule.html#ae5496eef565af191522a104122d70d75',1,'tpgmm.utils.learning_modules.ClassificationModule.davies_bouldin_score()']]]
+  ['davies_5fbouldin_5fscore_32',['davies_bouldin_score',['../classtpgmm_1_1utils_1_1learning__modules_1_1ClassificationModule.html#ae5496eef565af191522a104122d70d75',1,'tpgmm::utils::learning_modules::ClassificationModule']]]
 ];
